6caacaf363a4d0233ffa2b3cec76c81faaab4002
Kannen Joshi <kannen.joshi@gmail.com>
2023-09-29 00:35:50 +0100

    Create swapper.py
    Swap between tracks
    
    TEMP store playlists in a dict, will move to db or other external file

---
+++ swapper.py
@@ -0,0 +1,21 @@
+import spotipy
+
+
+# TEMP
+playlists = {
+    "tense1": "https://open.spotify.com/playlist/2idQqZl9rw63dcwdOrE4bL?si=4de1cf3b5be84e32",
+    "svavk4": "https://open.spotify.com/playlist/4G64d75bneOiUhacI7Z1YE?si=b6bf7350453646e3&pt=704e51cd2b3043416551f4063b872bb1"
+}
+
+
+def get_context_uri(name):
+    return f'spotify:album:{playlists[name][34:].split("?")[0]}'
+
+
+def swap_to(sp, context_uri=None, name=None):
+    if context_uri:
+        sp.start_playback(context_uri=context_uri)
+    elif name:
+        sp.start_playback(context_uri=get_context_uri(name))
+    else:
+        raise Exception("Enter a context_uri or name")

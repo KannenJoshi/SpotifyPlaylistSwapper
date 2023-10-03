fd748edbb3833c544318075ab3d961f2d65ec74f
Kannen Joshi <kannen.joshi@gmail.com>
2023-10-03 17:29:21 +0100

    Shows sql table data using PyQt6

--- interface.py
+++ interface.py
@@ -1,33 +1,52 @@
-from PyQt6.QWidgets import *
+from PyQt6.QtWidgets import *
 from PyQt6.QtSql import *
 from os.path import exists
 
 import sys
 
-import .data
+import data
 
-# https://www.youtube.com/watch?v=BVsh4mnucJA
+W = 1366
+H = 768
+
+#https://www.youtube.com/watch?v=DM8Ryoot7MI
+class MyApp(QWidget):
+    def __init__(self, width, height):
+        super().__init__()
+        self.width = width
+        self.height = height
+
+        self.tables_data = {"playlists": None, "bindings": None}
+
+        self.resize(width, height)
+        self.layout = QVBoxLayout()
+
+        self.db = QSqlDatabase.addDatabase("QSQLITE")
+        self.db.setDatabaseName(data.DB)
+        self.db.open()
+
+        for table in self.tables_data:
+            self.tables_data[table] = self.get_from_db(table)
+
+            t=QTableView()
+            t.setModel(self.tables_data[table])
+            self.layout.addWidget(t)
+
+
+        self.setLayout(self.layout)
+
+    def get_from_db(self, table_name):
+        table = QSqlTableModel(None, self.db)
+        table.setTable(table_name)
+        table.select()
+        print(table)
+        return table
+
+
 def main():
     app = QApplication([])
 
-    db = QSqlDatabase.addDatabase("QSQLITE")
-    db.setDatabaseName(data.DB)
-    db.open()
-
-    playlists = QSqlTableModel(None, db)
-    playlists.setTable("playlists")
-    playlists.select()
-
-    p_table = QTableView()
-    p_table.setModel(playlists)
-    p_table.view()
-
-    bindings = QSqlTableModel(None, db)
-    bindings.setTable("bindings")
-    bindings.select()
-
-    b_table = QTableView()
-    b_table.setModel(bindings)
-    b_table.view()
+    myApp = MyApp(W,H)
+    myApp.show()
 
     app.exec()

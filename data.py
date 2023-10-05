c412356aedbffff0314d2fc25506a58f555b3fab
Kannen Joshi <kannen.joshi@gmail.com>
2023-10-05 13:57:00 +0100

    Update to data methods and db

--- data.py
+++ data.py
@@ -60,19 +60,27 @@ def close_connection(conn, safe=True):
 
 def initialise(db_file=DB):
     conn = create_connection(db_file)
+    # _reset_all(conn)
     create_table(conn, sql_create_playlists_table)
     create_table(conn, sql_create_bindings_table)
     return conn
 
 
-def get_context_uri(url):
-    return f'spotify:album:{url[34:].split("?")[0]}'
+def get_context_uri(url: str):
+    return f"spotify:album:{url[34:].split('?')[0]}"
+
+
+def get_all_playlists(conn):
+    c = conn.cursor()
+    c.executescript("SELECT * FROM playlists")
+    return c.fetchall()
 
 
 def playlist_sql(name, url):
+    print(get_context_uri(url))
     return f"""
     INSERT INTO playlists (name, context_uri)
-    VALUES ({name}, {get_context_uri(url)})
+    VALUES ('{name}', '{get_context_uri(url)}');
     """
 
 
@@ -85,22 +93,42 @@ def add_playlist(conn, name, url):
 
 
 def add_many_playlists(conn, *data):
-    sql = []
+    sql = """BEGIN;"""
 
     for playlist in data:
-        sql.append(playlist_sql(*playlist))
+        sql += playlist_sql(*playlist) + ";"
+
+    sql += "COMMIT;"
 
     c = conn.cursor()
-    c.executemany(sql)
+    c.executescript(sql)
     conn.commit()
 
 
-def delete_playlist(name):
+def delete_playlist(conn, name=None, id=None):
     try:
         c = conn.cursor()
-        c.execute(f"DELETE FROM playlists WHERE name='{name}'")
+        key = "name" if name else "id" if id else ""
+        val = name if name else id if id else ""
+
+        c.execute(f"DELETE FROM playlists WHERE {key}='{val}'")
         conn.commit()
     except Error as e:
         print(e)
 
-close_connection(initialise())
+
+def _reset_all(conn):
+    c = conn.cursor()
+    c.executescript("""
+                    BEGIN;
+                    DROP TABLE IF EXISTS playlists;
+                    DROP TABLE IF EXISTS bindings;
+                    COMMIT;
+                    """)
+
+
+# conn = initialise()
+# add_many_playlists(conn, ("tense1", "https://open.spotify.com/playlist/2idQqZl9rw63dcwdOrE4bL?si=4de1cf3b5be84e32"), ("salvador", "https://open.spotify.com/playlist/4G64d75bneOiUhacI7Z1YE?si=b6bf7350453646e3&pt=704e51cd2b3043416551f4063b872bb1"))
+# delete_playlist(conn, "tense1")
+# delete_playlist(conn, "salvador")
+# close_connection(conn)

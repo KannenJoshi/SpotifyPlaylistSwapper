060049f1a161e86e5c98197250fc426599707c2b
Kannen Joshi <kannen.joshi@gmail.com>
2023-10-22 01:05:55 +0100

    midi init

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 import pygame as pg
+import pygame.midi
 
 from interface import main
+from midi import midi
 
 
 if __name__=='__main__':
     pg.init()
+    pg.midi.init()
     main()
+    pg.midi.quit()
     pg.quit()
     sys.exit()
